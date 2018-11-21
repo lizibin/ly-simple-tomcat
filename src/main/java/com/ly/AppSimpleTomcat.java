4d0b24eaa1c2476bee5564f6876d215e9d031406
lizibin <125720240@qq.com>
2018-11-21 18:48:14 +0800

    修复打包问题

--- src/main/java/com/ly/AppSimpleTomcat.java
+++ src/main/java/com/ly/AppSimpleTomcat.java
@@ -5,10 +5,18 @@ import java.util.Date;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages="com.ly.mapper")
-public class AppSimpleTomcat {
+public class AppSimpleTomcat  extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AppSimpleTomcat.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppSimpleTomcat.class, args);
 		System.out.println(new Date()+"----------------------把美女带回家案例启动成功-------------------->>>");
